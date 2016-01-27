92f9b53328448d4b20aa9ae2917fa8e14ca77d24
Mickael Roy <roy-mickael@live.fr>
2016-01-27 22:20:54 +0100

    Add top & bottom labels, implement responsive behaviour

--- jquery.stepProgressBar.js
+++ jquery.stepProgressBar.js
@@ -60,7 +60,7 @@
                 this.$el.addClass('step-progressbar-rounded');
 
             this.topLabelContainer = $('<div>');
-            this.topLabelContainer.addClass('step-progressbar-toplabel');
+            this.topLabelContainer.addClass('step-progressbar-toplabels');
             this.$el.append(this.topLabelContainer);
 
             this.barContainer = $('<div>');
@@ -74,7 +74,7 @@
             this.barElm.append(this.progressElm);
 
             this.bottomLabelContainer = $('<div>');
-            this.bottomLabelContainer.addClass('step-progressbar-bottomlabel');
+            this.bottomLabelContainer.addClass('step-progressbar-bottomlabels');
             this.$el.append(this.bottomLabelContainer);
 
             this._updateProgress();
@@ -87,15 +87,30 @@
                 this.stepsContainer.addClass('step-progressbar-steps');
                 this.stepsWrapper.append(this.stepsContainer);
 
+                this.toplabelWrapper = $('<span>');
+                this.toplabelWrapper.addClass('step-progressbar-labels-wrapper');
+                this.topLabelContainer.append(this.toplabelWrapper);
+                this.bottomlabelWrapper = $('<span>');
+                this.bottomlabelWrapper.addClass('step-progressbar-labels-wrapper');
+                this.bottomLabelContainer.append(this.bottomlabelWrapper);
+
                 this._calcStepsProgressValues();
+                this._findNextStep();
 
                 for(var i = 0; i < this.settings.steps.length; i++) {
-                    this.stepContainer = $('<span>');
-                    this.stepContainer.addClass('step-progressbar-step');
-                    this.stepsContainer.append(this.stepContainer);
+                    // for following method calls we suppose that steps are sorted, if it's not always the case we'll have to implementa sort function.
 
-                    this.stepContainer.css('left', this.settings.steps[i].progressValue + '%');
+                    // create progress bar step element
+                    this._buildStep(this.settings.steps[i], i);
+
+                    // create top label corresponding to that step
+                    this._buildTopLabel(this.settings.steps[i], i);
+
+                    // create bottom label corresponding to that step
+                    this._buildBottomLabel(this.settings.steps[i], i);
                 }
+
+                this._updateNextStepElm();
             }
         },
 
@@ -139,6 +154,8 @@
         setCurrentValue: function(value) {
             this.settings.currentValue = value;
             this._updateProgress();
+            this._findNextStep();
+            this._updateNextStepElm();
         },
 
         // To call a call a pseudo private method: 
@@ -154,7 +171,7 @@
          */
 
         //
-        _calcProgressValue: function() {
+        _calcCurrentProgressValue: function() {
             if(isNullOrUndefined(this.settings.steps) || (this.settings.steps && this.settings.steps.length <= 0)) {
                 this.progressValue = 100;
             } else {
@@ -214,9 +231,116 @@
         },
 
         //
+        _findNextStep: function() {
+            if(isNullOrUndefined(this.settings.steps) || (this.settings.steps && this.settings.steps.length <= 0))
+                return;
+            else if(isNullOrUndefined(this.settings.currentValue)) {
+                var minAndMax = this._retrieveMinAndMaxSteps();
+                if(minAndMax.maxStep) {
+                    this._setNextStep(minAndMax.maxStep);
+                    return minAndMax.maxStep;
+                } else if(minAndMax.minStep) {
+                    this._setNextStep(minAndMax.minStep);
+                    return minAndMax.minStep;
+                } else
+                    return;
+            } else {
+                var nextStep;
+                for(var i = 0; i < this.settings.steps.length; i++) {
+                    if(isNullOrUndefined(nextStep) && !isNullOrUndefined(this.settings.steps[i].value) && this.settings.steps[i].value > this.settings.currentValue)
+                        nextStep = this.settings.steps[i];
+                    else if(!isNullOrUndefined(nextStep) && !isNullOrUndefined(this.settings.steps[i].value) && this.settings.steps[i].value > this.settings.currentValue && this.settings.steps[i].value < nextStep.value)
+                        nextStep = this.settings.steps[i];
+                }
+                if(!isNullOrUndefined(nextStep))
+                    this._setNextStep(nextStep);
+
+                return nextStep;
+            }
+        },
+
+        //
+        _setNextStep: function(step) {
+            if(isNullOrUndefined(this.settings.steps) || (this.settings.steps && this.settings.steps.length <= 0))
+                return;
+            for(var i = 0; i < this.settings.steps.length; i++) {
+                this.settings.steps[i].isNextStep = false;
+            }
+            step.isNextStep = true;
+        },
+
+        //
+        _updateNextStepElm: function() {
+            if(isNullOrUndefined(this.settings.steps) || (this.settings.steps && this.settings.steps.length <= 0))
+                return;
+            for(var i = 0; i < this.settings.steps.length; i++) {
+                if(this.settings.steps[i].isNextStep == true && !isNullOrUndefined(this.settings.steps[i].stepElement)) {
+                    this.settings.steps[i].stepElement.addClass('step-progressbar-nextstep');
+                    this.settings.steps[i].topLabelElement.addClass('step-progressbar-nextstep');
+                    this.settings.steps[i].bottomLabelElement.addClass('step-progressbar-nextstep');
+                } else if((this.settings.steps[i].isNextStep == false || isNullOrUndefined(this.settings.steps[i].isNextStep)) && !isNullOrUndefined(this.settings.steps[i].stepElement)) {
+                    this.settings.steps[i].stepElement.removeClass('step-progressbar-nextstep');
+                    this.settings.steps[i].topLabelElement.removeClass('step-progressbar-nextstep');
+                    this.settings.steps[i].bottomLabelElement.removeClass('step-progressbar-nextstep');
+                }
+            }
+        },
+
+        //
         _updateProgress: function() {
             if(this.progressElm)
-                this.progressElm.css('width', this._calcProgressValue() + '%');
+                this.progressElm.css('width', this._calcCurrentProgressValue() + '%');
+        },
+
+        //
+        _buildStep: function(step, index) {
+            if(isNullOrUndefined(step) || (!isNullOrUndefined(step) && isNullOrUndefined(step.progressValue)))
+                return;
+
+            var stepElm = $('<span>');
+            stepElm.addClass('step-progressbar-step');
+            if(index == 0)
+                stepElm.addClass('step-progressbar-firststep');
+            if(index == this.settings.steps.length - 1)
+                stepElm.addClass('step-progressbar-laststep');
+            stepElm.css('left', step.progressValue + '%');
+            this.stepsContainer.append(stepElm);
+            step.stepElement = stepElm;
+        },
+
+        //
+        _buildTopLabel: function(step, index) {
+            if(isNullOrUndefined(step) || (!isNullOrUndefined(step) && isNullOrUndefined(step.progressValue)))
+                return;
+
+            var topLabelElm = $('<span>');
+            topLabelElm.addClass('step-progressbar-steplabel');
+            if(index == 0)
+                topLabelElm.addClass('step-progressbar-firststep');
+            if(index == this.settings.steps.length - 1)
+                topLabelElm.addClass('step-progressbar-laststep');
+            topLabelElm.css('left', step.progressValue + '%');
+            var label = step.topLabel ? step.topLabel : (step.value ? (step.value + this.settings.unit) : '');
+            topLabelElm.html(label);
+            this.toplabelWrapper.append(topLabelElm);
+            step.topLabelElement = topLabelElm;
+        },
+
+        //
+        _buildBottomLabel: function(step, index) {
+            if(isNullOrUndefined(step) || (!isNullOrUndefined(step) && isNullOrUndefined(step.progressValue)))
+                return;
+
+            var bottomLabelElm = $('<span>');
+            bottomLabelElm.addClass('step-progressbar-steplabel');
+            if(index == 0)
+                bottomLabelElm.addClass('step-progressbar-firststep');
+            if(index == this.settings.steps.length - 1)
+                bottomLabelElm.addClass('step-progressbar-laststep');
+            bottomLabelElm.css('left', step.progressValue + '%');
+            bottomLabelElm.html(step.bottomLabel);
+            this.bottomlabelWrapper.append(bottomLabelElm);
+            step.bottomLabelElement = bottomLabelElm;
         }
     };
 
